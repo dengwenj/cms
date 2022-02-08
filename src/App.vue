37bba899e32be1988ce45b1a5f7519a492d6815a
dengwenj <927561237@qq.com>
2022-02-08 23:40:12 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-  const message = 'hello vue'
+interface Hhh {
+  name: string
+  age: number
+  sex: string
+}
+
+const message = 'hello vue'
+const person: Hhh = {
+  name: 'dwj',
+  age: 22,
+  sex: '男'
+}
 </script>
 
 <template>
   <div>
     {{ message }}
+    {{ person }}
   </div>
 </template>
