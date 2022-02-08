1626b2f736454f32cbcce7a4084f8fa24b193b37
dengwenj <927561237@qq.com>
2022-02-09 00:06:59 +0800

    vuex的项目搭建

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+import { useStore } from 'vuex'
+
+const store = useStore()
+
 </script>
 
 <template>
   <div>
+    {{ store.state.count }}
     <router-view />
   </div>
 </template>
