4ea6bbe77a05eefea2f4a48460bede5539d7cfed
dengwenj <927561237@qq.com>
2022-02-08 23:42:47 +0800

    给路径取别名

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   lintOnSave: false,
   configureWebpack: {
+    // 给路径取别名
     resolve: {
       alias: {
-        assets: '@/assets'
-      }
-    }
-  }
+        assets: '@/assets',
+        components: '@/components',
+        router: '@/router',
+        pages: '@/pages',
+        store: '@/store',
+        api: '@/api',
+        utils: '@/utils',
+      },
+    },
+  },
 }
