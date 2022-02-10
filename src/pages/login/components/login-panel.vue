fdcce647a9e9075e82daeafce17b7bb5648afbc2
dengwenj <927561237@qq.com>
2022-02-10 19:29:33 +0800

    login的布局

---
+++ src/pages/login/components/login-panel.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import { ElTabs, ElTabPane, ElIcon } from 'element-plus'
+import { Cellphone, User } from '@element-plus/icons-vue'
+</script>
+
+<template>
+  <div class="login-panel">
+    <h1 class="title">CMS</h1>
+    <ElTabs type="border-card" stretch>
+      <ElTabPane>
+        <template #label>
+          <span class="header">
+            <el-icon><user /></el-icon>
+            <span :style="{ marginLeft: '5px' }">账号登录</span>
+          </span>
+        </template>
+        账号登录
+      </ElTabPane>
+      <ElTabPane>
+        <template #label>
+          <span class="header">
+            <el-icon><cellphone /></el-icon>
+            <span :style="{ marginLeft: '5px' }">手机登录</span>
+          </span>
+        </template>
+        手机登录
+      </ElTabPane>
+    </ElTabs>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.login-panel {
+  width: 320px;
+  .title {
+    text-align: center;
+  }
+  .header {
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
-
+import LoginPanel from './components/login-panel.vue'
 </script>
 
 <template>
   <div class="login">
-
+    <LoginPanel />
   </div>
 </template>
 
 <style scoped lang="less">
 .login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 100%;
   background: url(../../assets/img/jiankang.png) no-repeat;
   background-size: cover;
