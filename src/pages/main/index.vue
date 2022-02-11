b4e8b6322a49ee9c0fde01404c5683695e8988d6
dengwenj <927561237@qq.com>
2022-02-11 22:16:22 +0800

    布局

--- src/pages/main/index.vue
+++ src/pages/main/index.vue
@@ -1,9 +1,39 @@
 <script lang="ts" setup>
-
+import { ElContainer, ElAside, ElHeader, ElMain } from 'element-plus'
 </script>
 
 <template>
-  <div>
-    main
+  <div class="main">
+    <ElContainer class="main-content">
+      <ElAside class="main-aside" width="200px">Aside</ElAside>
+      <ElContainer class="page-all">
+        <ElHeader class="page-header">Header</ElHeader>
+        <ElMain class="page-content">Main</ElMain>
+      </ElContainer>
+    </ElContainer>
   </div>
 </template>
+
+<style lang="less" scoped>
+.main {
+  height: 100%;
+  .main-content {
+    height: 100%;
+    .main-aside {
+      background-color: #041225;
+    }
+    .page-all {
+      .page-header {
+        display: flex;
+        align-items: center;
+      }
+      .page-content {
+        display: flex;
+        justify-content: center;
+        background-color: #f1f0f5;
+      }
+    }
+  }
+}
+
+</style>
