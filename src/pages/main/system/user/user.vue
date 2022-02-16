6d9e9dee95468b80cd571b8407632722356692ec
dengwenj <927561237@qq.com>
2022-02-16 21:45:39 +0800

    用computed有依赖包裹下

--- src/pages/main/system/user/user.vue
+++ src/pages/main/system/user/user.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { computed, onUpdated } from 'vue'
+
+import { useStore } from '@/store'
 import PageSearch from '@/components/page-search'
 import PageContent from '@/components/page-content'
 import PageModal from '@/components/page-modal'
@@ -8,9 +11,12 @@ import modalConfig from './config/modal'
 import usePageSearch from '@/hooks/usePageSearch'
 import usePageModal from '@/hooks/usePageModal'
 
+const store = useStore()
+
 // hook
 const [pageContentRef, handleResetClick, handleSearchClick] = usePageSearch()
 
+// 密码是否显示隐藏
 const createCallback = () => {
   const passwordItem = modalConfig.formItem.find((item) => item.fieid === 'password')
   passwordItem!.isHidden = false
@@ -19,6 +25,16 @@ const editCallback = () => {
   const passwordItem = modalConfig.formItem.find((item) => item.fieid === 'password')
   passwordItem!.isHidden = true
 }
+// 当依赖发生改变的时候会执行里面的这个函数
+const modalConfigRef = computed(() => {
+  // 动态添加角色和部门的列表
+  const departmentItem = modalConfig.formItem.find((item) => item.fieid === 'departmentId')
+  departmentItem!.options = store.state.entireDepartment.map((item) => ({ title: item.name, value: item.id }))
+  const roleItem = modalConfig.formItem.find((item) => item.fieid === 'roleId')
+  roleItem!.options = store.state.entirRole.map((item) => ({ title: item.name, value: item.id }))
+  return modalConfig
+})
+// usePageModal hook
 const [
   pageModalRef,
   defaultInfo,
@@ -45,7 +61,7 @@ const [
     </div>
     <PageModal
       ref="pageModalRef"
-      :modalConfig="modalConfig"
+      :modalConfig="modalConfigRef"
       :defaultInfo="defaultInfo"
     />
   </div>
