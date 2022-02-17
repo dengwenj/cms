7adda68e9435b367e4db6a3909eb89b21c42ea79
dengwenj <927561237@qq.com>
2022-02-17 17:59:27 +0800

    echarts的实例

--- src/pages/main/analysis/dashboard/dashboard.vue
+++ src/pages/main/analysis/dashboard/dashboard.vue
@@ -1,7 +1,37 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import * as echarts from 'echarts'
 
+const divRef = ref<HTMLElement>()
+const option = {
+  title: {
+    text: 'ECharts 入门示例'
+  },
+  tooltip: {},
+  legend: {
+    data: ['销量']
+  },
+  xAxis: {
+    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+  },
+  yAxis: {},
+  series: [
+    {
+      name: '销量',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }
+  ]
+};
+
+onMounted(() => {
+ const echartInstance = echarts.init(divRef.value!)
+  echartInstance.setOption(option)
+})
 </script>
 
 <template>
-  <div class="dashboard">dashboard</div>
+  <div class="dashboard">
+    <div ref="divRef" style="width: 600px; height: 500px;"></div>
+  </div>
 </template>
